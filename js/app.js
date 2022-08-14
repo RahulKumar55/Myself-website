e9a33b8ec9e49fdd6be50cd57e0a2781c177a6df
unknown <manni7728449@gmail.com>
2022-08-14 05:08:24 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var i = 0;
+function add() {
+  if (i === 0) {
+    var inp = document.createElement("input");
+    inp.setAttribute("id", "rate");
+    inp.setAttribute("type", "text");
+    inp.setAttribute("pattern", "[0-9]+.[0-9]{2}");
+
+    var lab = document.createElement("label");
+    lab.setAttribute("for", "rate");
+    lab.innerHTML = "HOURLY RATE:";
+
+    var pay = document.getElementById("pay");
+    pay.appendChild(lab);
+    pay.appendChild(inp);
+    i = 1;
+  }
+}
+
+function subtract() {
+  var pay = document.getElementById("pay");
+  pay.innerHTML = "";
+  i = 0;
+}
+
+add();
+subtract();
